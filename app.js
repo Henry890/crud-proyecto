5b5a57c96558a760f376f2b8abfca0aea23ede59
Henry890 <henryromerom530@gmail.com>
2025-11-24 18:54:17 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+function listarUsuarios() {
+  const data = fs.readFileSync("./crud/usuarios.json", "utf8");
+  console.log(JSON.parse(data));
+}
+
+listarUsuarios();
+
+
+
+
+
+
+console.log("Funcionalidad nueva3");
