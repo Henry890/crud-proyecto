1f1298660be88cb562b8c0330db501b00279c8b4
Henry890 <henryromerom530@gmail.com>
2025-11-24 18:39:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const fs = require("fs");
+
+function listarUsuarios() {
+  const data = fs.readFileSync("./crud/usuarios.json", "utf8");
+  console.log(JSON.parse(data));
+}
+
+listarUsuarios();
+
 console.log("Funcionalidad nueva");
